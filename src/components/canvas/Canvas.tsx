55e535e5cf0a32ee8fbe6c374a045ad85bdba879
olgamomirov <olgamomirov@gmail.com>
2023-10-19 02:45:35 +0200

    Trying react-draggable

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Palette from "../palette/Palette";
 import ImageHolder from "../image/ImageHolder";
+import Draggable from "react-draggable";
 
 function Canvas() {
   return (
     <div className="flex w-4/5 h-4/5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white flex-col desktop:flex-row">
-      <Palette />
+      <Draggable axis="both" handle=".handle" grid={[25, 25]} scale={1}>
+        <div className="flex flex-wrap justify-center w-[200px] h-[auto] absolute bottom-0 right-0 handle">
+          <Palette />
+        </div>
+      </Draggable>
+
       <ImageHolder />
     </div>
   );
